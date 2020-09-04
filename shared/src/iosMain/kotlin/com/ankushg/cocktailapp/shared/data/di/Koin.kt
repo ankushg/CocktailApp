fb0fd14a26c519beaa188f0203c10029bf7358e9
Ankush Gupta <me@ankushg.com>
2020-09-04 13:51:01 -0700

    configure ios/android SqlDrivers

--- shared/src/iosMain/kotlin/com/ankushg/cocktailapp/shared/data/di/Koin.kt
+++ shared/src/iosMain/kotlin/com/ankushg/cocktailapp/shared/data/di/Koin.kt
@@ -1,8 +1,16 @@
 package com.ankushg.cocktailapp.shared.data.di
 
 import com.ankushg.cocktailsapp.CocktailsDb
+import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val platformDataModule: Module = module {
+    single<SqlDriver> {
+        NativeSqliteDriver(
+            CocktailsDb.Schema,
+            get()
+        )
+    }
 }
