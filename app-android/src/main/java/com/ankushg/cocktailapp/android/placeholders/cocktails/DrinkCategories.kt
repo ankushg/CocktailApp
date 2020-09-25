6d9172473b7fbbc9c5baf1b2867d7fc049c92ca9
Ankush Gupta <me@ankushg.com>
2020-09-24 19:18:10 -0700

    remove old placeholders

--- app-android/src/main/java/com/ankushg/cocktailapp/android/placeholders/cocktails/DrinkCategories.kt
+++
@@ -1,5 +0,0 @@
-package com.ankushg.cocktailapp.android.placeholders.cocktails
-
-import com.ankushg.cocktailapp.shared.data.enums.DrinkCategory
-
-val allDrinkCategories = DrinkCategory.values().toList()
