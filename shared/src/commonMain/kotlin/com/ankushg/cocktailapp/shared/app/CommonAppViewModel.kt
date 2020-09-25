80943e68e9b640762a39b88ce457210453e4e5a9
Ankush Gupta <me@ankushg.com>
2020-09-24 19:13:49 -0700

    placeholder action impls

--- shared/src/commonMain/kotlin/com/ankushg/cocktailapp/shared/app/CommonAppViewModel.kt
+++ shared/src/commonMain/kotlin/com/ankushg/cocktailapp/shared/app/CommonAppViewModel.kt
@@ -7,6 +7,7 @@ import com.ankushg.cocktailapp.shared.data.placeholders.cocktailSummaries
 import com.ankushg.cocktailapp.shared.data.placeholders.margarita
 import com.ankushg.cocktailapp.shared.data.placeholders.vodka
 import com.ankushg.cocktailapp.shared.domain.entities.recipeIngredients
+import com.ankushg.cocktailapp.shared.utils.CStateFlow
 import com.ankushg.cocktailapp.shared.utils.Navigator
 import com.ankushg.cocktailapp.shared.utils.wrap
 import kotlinx.coroutines.MainScope
@@ -32,7 +33,13 @@ class CommonAppViewModel(
     override fun submitAction(action: Action) {
         log.d { "Received action: $action" }
 
-        val newState: ViewState? = null // TODO
+        val newState: ViewState? = when (action) {
+            is Action.SelectCategory -> processCategorySelection(action)
+            is Action.SelectDrink -> processDrinkSelection(action)
+            is Action.SelectIngredient -> processIngredientSelection(action)
+            Action.UpPress -> processUpPress()
+            Action.BackPress -> processBackPress()
+        }
 
         emitState(newState)
     }
@@ -41,5 +48,25 @@ class CommonAppViewModel(
         if (newState != null && newState != mutableStateFlow.value) {
             mutableStateFlow.value = newState
         }
+    }
+
+    private fun processCategorySelection(selectCategory: Action.SelectCategory): ViewState? {
+        return ViewState.CocktailList(cocktailSummaries)
+    }
+
+    private fun processDrinkSelection(selectDrink: Action.SelectDrink): ViewState? {
+        return ViewState.DrinkDetails(margarita, margarita.recipeIngredients)
+    }
+
+    private fun processIngredientSelection(selectIngredient: Action.SelectIngredient): ViewState? {
+        return ViewState.IngredientDetails(vodka, cocktailSummaries)
+    }
+
+    private fun processUpPress(): ViewState? {
+        return null // TODO: nav
+    }
+
+    private fun processBackPress(): ViewState? {
+        return null // TODO: nav
     }
 }
